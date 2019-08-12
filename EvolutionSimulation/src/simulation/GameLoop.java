639d0178d33ce683a25e7b64f93ac60e92d65f72
Bram <eckelradekip10@gmail.com>
2019-08-12 21:54:37 +0100

    Time is now saved in seconds not in miliseconds

--- EvolutionSimulation/src/simulation/GameLoop.java
+++ EvolutionSimulation/src/simulation/GameLoop.java
@@ -70,7 +70,7 @@ public class GameLoop implements ActionListener{
 		averageData.setAvgAge(environment.getMaxAgeStats()[0]);
 		averageData.setAvgScent(environment.getScentStats()[0]);
 		averageData.setAvgEnergyCost(environment.getEnergyConsumptionStats()[0]);
-		averageData.setTime(timeElapsed);
+		averageData.setTime(timeElapsed/1000);
 	}
 	
 		private void addPopData() {
@@ -81,7 +81,7 @@ public class GameLoop implements ActionListener{
 				popData[i].setAvgAge(sp.getMaxAgeStats()[0]);
 				popData[i].setAvgScent(sp.getScentStats()[0]);
 				popData[i].setAvgEnergyCost(sp.getEnergyConsumptionStats()[0]);
-				popData[i].setTime(timeElapsed);
+				popData[i].setTime(timeElapsed/1000);
 				popData[i].setNrSpecies(sp.getNrSpecies());
 			}
 		}
