21603722d4d7d6538fe17c3627da2c41f1f742a8
Bram <eckelradekip10@gmail.com>
2019-09-02 13:08:59 +0100

    made start on test case but first going to fix some bugs

--- EvolutionSimulation/src/tests/TestSpecies.java
+++ EvolutionSimulation/src/tests/TestSpecies.java
@@ -1,6 +1,7 @@
 package tests;
 
 import junit.framework.TestCase;
+import species.Carnivore;
 import species.Plant;
 import species.Species;
 
@@ -10,7 +11,7 @@ public class TestSpecies extends TestCase {
 		Species s = new Plant(10, 10, 2000);
 		s.setXYLoc(100, 100);
 		int[] test = s.getOfsetXYLoc();
-		//test 1000 times
+		//test 100 times
 		for (int i =0; i < 100; i++) {
 			assertTrue(test[0] >= 50 && test[0] <= 150 && test[1] >= 50 && test[1] <= 150);
 		}
@@ -19,10 +20,15 @@ public class TestSpecies extends TestCase {
 		Species s = new Plant(10, 10, 2000);
 		s.setXYLoc(900, 900);
 		int[] test = s.getOfsetXYLoc();
-		//test 1000 times
+		//test 100 times
 		for (int i =0; i < 100; i++) {
 			assertTrue(test[0] >= 850 && test[0] <= 950 && test[1] >= 850 && test[1] <= 950);
 		}
+	}
+	
+	public void testCanMultiply() {
+		Species s = new Carnivore(10, 10, 10, 10, 1.0);
+		
 	}
 
 }
