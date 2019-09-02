18dc3c182104c72a363f272eb048b0dabb43287c
Bram <eckelradekip10@gmail.com>
2019-09-02 11:26:39 +0100

    added some extra tests

--- EvolutionSimulation/src/tests/AutotrophPopulationTest.java
+++ EvolutionSimulation/src/tests/AutotrophPopulationTest.java
@@ -9,16 +9,32 @@ import species.AutotrophSpecies;
 
 public class AutotrophPopulationTest extends TestCase {
 	
-	public void testIsOverlapping() {
+	public void testIsOverlappingPositive() {
 		AutotrophPopulation p = new AutotrophPopulation(Color.GREEN, "Plant", "a nanme");
 		AutotrophSpecies s = new AutotrophSpecies(10, 10, 2000);
 		p.addSpecies(s);
 		p.getSpecies(0).setXYLoc(10, 10);
-		assertEquals(p.isOverlapping(10, 10), true);
-		assertEquals(p.isOverlapping(0, 29), true);
-		assertEquals(p.isOverlapping(29, 0), true);
-		assertEquals(p.isOverlapping(30, 0), false);
-		assertEquals(p.isOverlapping(0, 30), false);
+		assertEquals(true, p.isOverlapping(10, 10));
+		//just inside region
+		assertEquals(true, p.isOverlapping(10, 29));
+		assertEquals(true, p.isOverlapping(29, 10));
+		//just outside
+		assertEquals(false, p.isOverlapping(30, 10));
+		assertEquals(false, p.isOverlapping(10, 30));
+	}
+	
+	public void testIsOverlappingNegative() {
+		AutotrophPopulation p = new AutotrophPopulation(Color.GREEN, "Plant", "a nanme");
+		AutotrophSpecies s = new AutotrophSpecies(10, 10, 2000);
+		p.addSpecies(s);
+		p.getSpecies(0).setXYLoc(100, 100);
+		assertEquals(true, p.isOverlapping(100, 100));
+		//just inside region
+		assertEquals(true, p.isOverlapping(100, 81));
+		assertEquals(true, p.isOverlapping(81, 100));
+		//just outside
+		assertEquals(false, p.isOverlapping(80, 100));
+		assertEquals(false, p.isOverlapping(100, 80));
 	}
 
 }
