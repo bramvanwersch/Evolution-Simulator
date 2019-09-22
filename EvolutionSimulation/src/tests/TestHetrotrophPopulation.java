d4bc5971afa7c09212b357634bd1d1193e7285b1
Bram <eckelradekip10@gmail.com>
2019-09-22 10:06:37 +0100

    Added 2 test cases for cloning and normal reproduction.

--- EvolutionSimulation/src/tests/TestHetrotrophPopulation.java
+++ EvolutionSimulation/src/tests/TestHetrotrophPopulation.java
@@ -1,7 +1,34 @@
 package tests;
 
+import java.awt.Color;
+
 import junit.framework.TestCase;
+import populations.HetrotrophPopulation;
+import species.Herbivore;
+import species.HetrotrophSpecies;
 
 public class TestHetrotrophPopulation extends TestCase {
+	
+	public void testCreateOffspring() {
+		HetrotrophPopulation p = new HetrotrophPopulation(Color.GREEN, "Herbivore", "some name");
+		HetrotrophSpecies h = new Herbivore(10, 10, 10, 10, 1.0);
+		p.addSpecies(h);
+		p.createOffspring(0);
+		assertEquals(2, p.getNrSpecies());
+	}
+	
+	public void testCloneOffspring() {
+		HetrotrophPopulation p = new HetrotrophPopulation(Color.GREEN, "Herbivore", "some name");
+		HetrotrophSpecies h = new Herbivore(10, 10, 10, 10, 1.0);
+		p.addSpecies(h);
+		p.cloneOffspring(0);
+		HetrotrophSpecies sp1 = p.getSpecies(0);
+		HetrotrophSpecies sp2 = p.getSpecies(1);
+		
+		assertEquals(sp1.getMaxAge(), sp2.getMaxAge());
+		assertEquals(sp1.getMaxSize(), sp2.getMaxSize());
+		assertEquals(sp1.getScentRange(), sp2.getScentRange());
+		assertEquals(sp1.getSpeed(), sp2.getSpeed());
+	}
 
 }
