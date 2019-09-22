195edab4601c0ac2044b8269d768aa9299a17141
Bram <eckelradekip10@gmail.com>
2019-09-22 09:50:08 +0100

    Finished annotating

--- EvolutionSimulation/src/populations/HetrotrophPopulation.java
+++ EvolutionSimulation/src/populations/HetrotrophPopulation.java
@@ -17,19 +17,26 @@ import species.Species;
  * @author Bram van Wersch
  */
 public class HetrotrophPopulation extends Population {
+	//List of all species that can grow or shrink depending on survival of 
+	//the species
 	private ArrayList<HetrotrophSpecies> speciesList;
 
 	/**
-	 * 
-	 * @param color
-	 * @param type
-	 * @param name
+	 * Constructor that creates the list of species that each population holds.
+	 * @param color of the species as can be seen in the terrainPanel
+	 * @param type of species for interactions within the ecosystem
+	 * @param name as given by the player of the game to the species.
 	 */
 	public HetrotrophPopulation(Color color, String type, String name) {
 		super(color, type, name);
 		this.speciesList = new ArrayList<HetrotrophSpecies>();
 	}
 	
+	/**
+	 * Triggers merhods that happen each update of the simulation. It triggers
+	 * the nextTimePoint methods for all species aswell as a check for multiplying 
+	 * and ability of the species to survive.
+	 */
 	@Override
 	public void nextTimePoint() {
 		for (int i = 0; i < getNrSpecies(); i++) {
@@ -39,31 +46,58 @@ public class HetrotrophPopulation extends Population {
 		checkAliveSpecies();
 	}
 	
+	/**
+	 * Adds a species to the end of the speciesList
+	 * @param s is the instance of a hetrotrophspecies to be added to the list.
+	 */
 	public void addSpecies(HetrotrophSpecies s) {
 		this.speciesList.add(s);
 	}
 	
+	/**
+	 * Return the size of the species list
+	 */
 	@Override
 	public int getNrSpecies() {
 		return speciesList.size();
 	}
 	
+	/**
+	 * Returns a species at a certain index of the list. This construct
+	 * tries to prevent the user handelig the full list but instead the 
+	 * individual species.
+	 * @param index of the species to be returned
+	 */
 	@Override
 	public HetrotrophSpecies getSpecies(int index) {
 		return speciesList.get(index);
 	}
 	
+	/**
+	 * Removes a species from a certain index
+	 * @param index of the species to be removed
+	 */
 	@Override
 	public void removeSpecies(int index) {	
 		speciesList.remove(index);
 		addDiedSpiecies();
 	}
 	
+	/**
+	 * Shuffles the list of species making sure that the order in which
+	 * the species are processed is different for every update
+	 */
 	@Override
 	public void shuffleSpeciesList() {
 		Collections.shuffle(speciesList);
 	}
 	
+	/**
+	 * Function that creates offspring by taking a species at a certain index and 
+	 * taking its genome and mutate it. After that the values for the attributes are 
+	 * slightly changed. Also the location is slightly changed.
+	 * @param index of the species to be multiplied.
+	 */
 	@Override
 	public void createOffspring(int index) {
 		HetrotrophSpecies s = speciesList.get(index);
@@ -98,6 +132,11 @@ public class HetrotrophPopulation extends Population {
 		
 	}
 	
+	/**
+	 * Clones a species at a given index by copying the genome. The starting location
+	 * of the clones spcies is completely random within the bounds of the playing field.
+	 * @param index of the species to be cloned.
+	 */
 	@Override
 	public void cloneOffspring(int index) {
 		HetrotrophSpecies s = speciesList.get(index);
@@ -130,6 +169,9 @@ public class HetrotrophPopulation extends Population {
 		
 	}
 
+	/**
+	 * Needs some work to work again. Was a function for recording the data of species.
+	 */
 	@Override
 	public void addSpeciesData(Species s, int prevNumber) {
 //		String data = getSpeciesData();
